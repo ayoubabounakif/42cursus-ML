48cbdc8a25ffc1479bde59cc0c11e91f9f004358
ABOUNAKIF Ayoub <aabounakif@leyton.com>
2023-02-13 19:12:51 +0100

    [ML-01][WIP] Test MyLR.mse_ with sklearn mse

--- module_01/ex04/linear_model.py
+++ module_01/ex04/linear_model.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
-# from sklearn.metrics import mean_squared_error
 from importlib.machinery import SourceFileLoader
+from sklearn.metrics import mean_squared_error
+
 
 MyLinearRegression = SourceFileLoader("MyLinearRegression", "../ex03/my_linear_regression.py").load_module().MyLinearRegression
-
 
 def main():
   data = pd.read_csv("are_blue_pills_magics.csv")
@@ -16,8 +16,10 @@ def main():
   Y_model1 = linear_model1.predict_(Xpill)
   Y_model2 = linear_model2.predict_(Xpill)
 
-  print(MyLinearRegression.mse_(Yscore, Y_model1)) # 57.60304285714282
-  print(MyLinearRegression.mse_(Yscore, Y_model2)) # 232.16344285714285
+  print(MyLinearRegression.mse_(Yscore, Y_model1)) # 57.603042857142825
+  print(mean_squared_error(Yscore, Y_model1)) # 57.6030428571428255
+  print(MyLinearRegression.mse_(Yscore, Y_model2)) # 232.16344285714283
+  print(mean_squared_error(Yscore, Y_model2)) # 232.16344285714283
 
 
 
